f40ad05f42422dc0f838f4d338a581e591b895de
Outrun Colors, LLC <outruncolors@gmail.com>
2021-10-27 02:26:46 -0500

    Add surreal asset

---
+++ src/components/surreal/atomic/Asset.tsx
@@ -0,0 +1,71 @@
+import { PLACEHOLDER_TOKEN_IMAGE } from "config";
+import { Typography } from "antd";
+import { useMemo } from "react";
+
+interface Props {
+  address?: string;
+  showImage?: boolean;
+  symbol: string;
+}
+
+export function SurrealAsset({
+  showImage = true,
+  symbol,
+  address = "",
+}: Props) {
+  const image = useMemo(() => determineImagePath(symbol, address), [
+    symbol,
+    address,
+  ]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      {showImage && (
+        <img
+          src={image}
+          alt={symbol}
+          style={{
+            width: 36,
+            height: 36,
+            marginRight: "1rem",
+          }}
+        />
+      )}
+      <Typography.Title
+        level={3}
+        style={{
+          textTransform: "uppercase",
+          marginTop: 0,
+          marginBottom: 0,
+          position: "relative",
+          top: 2,
+        }}
+      >
+        {symbol}
+      </Typography.Title>
+    </div>
+  );
+}
+
+// #region Helpers
+function determineImagePath(symbol: string, address?: string) {
+  const path = address
+    ? `https://tokens.dharma.io/assets/${address.toLowerCase()}/icon.png`
+    : "";
+
+  if (path) {
+    return path;
+  } else {
+    try {
+      return require(`images/${symbol.toLowerCase()}.png`).default;
+    } catch {
+      return PLACEHOLDER_TOKEN_IMAGE;
+    }
+  }
+}
+// #endregion
--- src/components/surreal/routes/Vaults.tsx
+++ src/components/surreal/routes/Vaults.tsx
@@ -1,14 +1,26 @@
-import { SurrealHeading, SurrealPanel } from "../atomic";
+import {
+  SurrealAsset,
+  SurrealHeading,
+  SurrealPanel,
+} from "components/surreal/atomic";
 
 export default function SurrealVaultsRoute() {
   return (
-    <div style={{ position: "fixed", top: "20rem", left: "20rem" }}>
+    <div
+      style={{
+        position: "fixed",
+        top: "20rem",
+        left: "20rem",
+        minWidth: "60vw",
+      }}
+    >
       <SurrealPanel>
         <SurrealHeading
           image="http://via.placeholder.com/100"
           title="Vaults"
           subtitle="Earn Interest on Your Assets"
         />
+        <SurrealAsset symbol="WBTC" />
       </SurrealPanel>
     </div>
   );
